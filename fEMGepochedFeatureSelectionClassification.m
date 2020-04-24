d782e92dcfa283fe1a486d6a78ff08bb8e4f6b70
Anusha Datar <anushadatar@gmail.com>
2020-04-23 21:06:10 -0400

    added maximum fractal length feature

--- fEMGepochedFeatureSelectionClassification.m
+++ fEMGepochedFeatureSelectionClassification.m
@@ -141,10 +141,10 @@ w.alltimewindowsforfeatures = [w.starttimes; w.endtimes]; %(:,1) for first pair
 
 %includedfeatures = {'bp2t20','bp40t56','bp64t80' ,'bp80t110'};
 %includedfeatures = {'bp2t20','bp20t40','bp40t56','bp64t80' ,'bp80t110','rms', 'iemg','mmav1','var','mpv','var','ssi'};
-% includedfeatures = {'bp40t56','bp64t80' ,'bp80t110','rms', 'iemg','mmav1','var'};
-%includedfeatures = {'rms', 'absmean','ssi','iemg','mmav1','mpv','var'}; %names of included features in the data table
-%includedfeatures = {'rms', 'iemg','mmav1','var'}; %names of included features in the data table
-includedfeatures = {'bp40t56','bp64t80' ,'bp80t110','rms', 'iemg','mmav1','var', 'medianfreq'};
+includedfeatures = {'bp40t56','bp64t80' ,'bp80t110','rms', 'iemg','mmav1','var'};
+% includedfeatures = {'rms', 'absmean','ssi','iemg','mmav1','mpv','var'}; %names of included features in the data table
+%includedfeatures = {'rms', 'iemg','mmav1','var', 'mfl'}; % Include mfl
+% includedfeatures = {'bp40t56','bp64t80' ,'bp80t110','rms', 'iemg','mmav1','var', 'medianfreq'};
 includedchannels = 1:2; %channels to included, this will calculate features for each separately 
 %(if you have cross channel features, you need to write something in to
 %skip in order to avoid repeat features)
@@ -217,6 +217,8 @@ for ttround = 1:2
                         fvalues = [fvalues bandpower(squeeze(EEG.data(ch,timewindowepochidx,idxt)),EEG.srate,[80 110])'];
                     case 'medianfreq'
                         fvalues = [fvalues squeeze(real(median(fft(EEG.data(ch,timewindowepochidx,idxt), '', 2), 2)))];
+                    case 'mfl'
+                        fvalues = [fvalues squeeze(real(log10(sqrt(sum(diff(EEG.data(ch,timewindowepochidx,idxt)).^2, 2)))))];
                     otherwise
                         disp(strcat('unknown feature: ', includedfeatures{f},', skipping....'))
                 end
